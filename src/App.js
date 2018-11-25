cd32412538e930fe99ae6227cc2a8b329316d0c3
Moulin Philippe <philippe.moulin@gfi.fr>
2018-11-25 23:40:36 +0100

    reactCourse: wrap with ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import classes from './App.css'
 import Person from './Person/Person'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
   state = {
@@ -42,6 +43,7 @@ class App extends Component {
   deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice()
     const persons = [...this.state.persons]
+
     persons.splice(personIndex, 1)
     this.setState({ persons: persons })
   }
@@ -60,13 +62,14 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={event => this.nameChangedHandler(event, person.id)}
-              />
+              <ErrorBoundary key={person.id}>
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age}
+                  changed={event => this.nameChangedHandler(event, person.id)}
+                />
+              </ErrorBoundary>
             )
           })}
         </div>
